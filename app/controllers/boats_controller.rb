ce04bfb153e3d0269982f12f92ae3f16aef93f73
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2020-11-16 18:26:24 +0100

    boats controller done and boats views done

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,46 @@
+class BoatsController < ApplicationController
+
+  def index
+    @boats = Boat.all
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.user = current_user
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
+  def edit
+    @boat = Boat.find(params[:id])
+  end
+
+  def update
+    @boat = Boat.find(params[:id])
+    @boat.update(boat_params)
+    redirect_to boat_path(@boat)
+  end
+
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
+    redirect_to boats_path
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:user, :model, :name, :description, :capacity, :price_per_day)
+  end
+end
