562dedd3409eaae46722fea561576c7f4a4a6a49
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2020-11-18 15:13:56 +0100

    boat model photos validation

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,6 +3,6 @@ class Boat < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
 
-  validates :name, :boat_model, :capacity, :description, :price_per_day, presence: true
+  validates :name, :boat_model, :capacity, :description, :price_per_day, :photos, presence: true
   validates :capacity, numericality: true
 end
